84af1f332f59eabd616117febe35e4b860ee7309
Anton Pomogaev <antonpomogaev@gmail.com>
2022-11-30 17:06:21 +0300

    add solution to fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import { mkdir, readdir, copyFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const pathToDestDir = join(__dirname, 'files_copy');
+        const pathToSrcDir = join(__dirname, 'files');
+
+        const entries = await readdir(pathToSrcDir);
+        await mkdir(pathToDestDir);
+        
+        for (const entry of entries) {
+            const src = join(pathToSrcDir, entry);
+            const dest = join(pathToDestDir, entry);
+            copyFile(src, dest);
+        }
+    } catch {
+        try {
+            throw new Error('FS operation failed');
+        } catch (error) {
+            console.error(error);
+        }
+    }
 };
 
 copy();
