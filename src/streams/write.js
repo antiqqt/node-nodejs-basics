b810f55e97c09d99a20c2b16568f5ceada09aa7e
Anton Pomogaev <antonpomogaev@gmail.com>
2022-12-02 17:37:10 +0300

    refactor tasks solution to streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,13 +1,20 @@
 import { createWriteStream } from 'node:fs';
 import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
 import { dirname, join } from 'path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const { stdin } = process;
 
 const write = async () => {
-  const ws = createWriteStream(join(__dirname, 'files', 'fileToWrite.txt'));
-  stdin.pipe(ws);
+  try {
+    await pipeline(
+      stdin,
+      createWriteStream(join(__dirname, 'files', 'fileToWrite.txt'))
+    );
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 await write();
