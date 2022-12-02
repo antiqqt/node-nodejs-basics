23986f58604413360f54faef603dcf3ca4c8b919
Anton Pomogaev <antonpomogaev@gmail.com>
2022-12-02 17:18:32 +0300

    add solution to streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { createReadStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const { stdout } = process;
+
 const read = async () => {
-    // Write your code here 
+  const rs = createReadStream(join(__dirname, 'files', 'fileToRead.txt'));
+  rs.pipe(stdout);
 };
 
 await read();
